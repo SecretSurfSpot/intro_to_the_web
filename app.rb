825a8f1c3ae3236fa8594cf28c7f5a0a572cf552
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-20 15:19:24 +0100

    Adding app.rb with 3 pages, inc cat pic

--- app.rb
+++ app.rb
@@ -1,1 +1,16 @@
 require 'sinatra'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Welcome to the secret world"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+
+end

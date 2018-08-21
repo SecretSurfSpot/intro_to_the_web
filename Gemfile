82d3bf21d50486218b3a6e091ce06e387309ecd1
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-21 17:03:48 +0100

    Add cat_naming.erb, refactored index.erb, add post route to app.rb

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "https://rubygems.org"
 
+gem 'capybara'
+gem 'selenium-webdriver'
 gem 'sinatra'
 gem 'shotgun'
 gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,20 +1,22 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
-get '/' do
-  "Hello World"
-end
 
-get '/secret' do
-  "Welcome to the secret world"
-end
-
-get '/random-cat' do
-  @name = ['Amigo', 'Oscar', 'Viking'].sample
-  erb(:index)
-end
-
-get '/named-cat' do
-  p params[:name]
+get('/') {"HELO GOODBYE"}
+get('/secret') { 'Here is a secret page'}
+get('/apple') { 'Here is yet another page'}
+get('/pear') { 'Today is monday'}
+get('/orange') { 'Tomorrow is Tuesday'}
+get('/blueberry') { 'The day after tomorrow is Wednesday'}
+get('/random-cat') {
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+  }
+post('/named-cat') {
+  p params
   @name = params[:name]
-  erb:index
-end
+  erb :index
+  }
+get('/cat-naming-form') {
+  erb :cat_naming
+}
